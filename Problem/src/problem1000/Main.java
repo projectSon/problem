48f1f636e8ad528ff936523e6ac2bd264336aff2
Jieun Son <Jieun Son@DESKTOP-H9VIS55>
2021-02-28 23:42:48 +0900

    백준 1000번

---
+++ Problem/src/problem1000/Main.java
@@ -0,0 +1,23 @@
+/*
+ * 백준 1000번 문제
+ */
+package problem1000;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s = new Scanner(System.in);
+		String str = s.next();
+		int a = Integer.parseInt(str);
+		str = s.next();
+		int b = Integer.parseInt(str);
+		
+		System.out.println(a+b);
+	}
+
+
+}

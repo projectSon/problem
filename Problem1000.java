1d222e30afc5fa95f0c755f56e5a663a7741d2a3
projectSon <je_son98@naver.com>
2021-03-03 12:49:24 +0900

    백준 1000번 문제

---
+++ Problem1000.java
@@ -0,0 +1,24 @@
+/*
+ * 백준 1000번 문제
+ * 2021.3.1
+ */
+package problem1000;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// a, b값 받기
+		Scanner s = new Scanner(System.in);
+		String str = s.next();
+		int a = Integer.parseInt(str);
+		str = s.next();
+		int b = Integer.parseInt(str);
+		
+		System.out.println(a+b);
+	}
+	
+}
